d3819e8031b7e7efb86852ae338b2c81150624b9
Pawel Bogusz1 <pbog2000@gmail.com>
2023-05-15 22:43:39 +0200

    jason

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
+
+import Profile from './profile/Profile';
+
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendsList friends={friends} />
+      <TransactionHistory items={transaction} />
+
+   
+    </>
   );
 };
+export default App;
