c4d5303da64ce95035625fe3ddc837e59204ce18
Pawel Bogusz1 <pbog2000@gmail.com>
2023-05-16 22:29:41 +0200

    update2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,17 +3,19 @@ import Profile from './Profile/Profile';
 import user from './Profile/user.json';
 
 
+
 const App = () => {
   return (
     <>
       <Profile
-        username={user.name}
+        name={user.name}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
         stats={user.stats}
       />
-   
+      
+ 
 
     </>
   );
