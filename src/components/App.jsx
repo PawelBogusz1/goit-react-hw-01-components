98f601894343333f8a1fb4521e1f3a02598448cb
Pawel Bogusz1 <pbog2000@gmail.com>
2023-05-16 21:09:54 +0200

    update1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,22 @@
-
-import Profile from './profile/Profile';
+import React from 'react';
+import Profile from './Profile/Profile';
+import user from './Profile/user.json';
 
 
 const App = () => {
   return (
     <>
       <Profile
-        name={user.name}
+        username={user.name}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
         stats={user.stats}
       />
-      <Statistics title="Upload stats" stats={data} />
-      <FriendsList friends={friends} />
-      <TransactionHistory items={transaction} />
-
    
+
     </>
   );
 };
+
 export default App;
